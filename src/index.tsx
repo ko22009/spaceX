b5fb89294f4ad7a4d2d0445630dec19fd8eabac0
ko2 <ko22009@yandex.ru>
2022-08-05 18:05:25 +0500

    add saga, dnd, antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import App from "./app";
-import { store } from "./store";
+import store from "./store";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
---
+++ src/store/index.ts
@@ -0,0 +1,23 @@
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+
+import rootSaga from "./sagas";
+import RootReducer from "./slices";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = configureStore({
+  reducer: {
+    reducer: RootReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      thunk: false,
+    }).concat(sagaMiddleware),
+});
+
+sagaMiddleware.run(rootSaga);
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
